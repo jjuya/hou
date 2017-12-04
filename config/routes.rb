d1efcfc1c3c4cbe31e02696e8b732d9720dec2ec
jjuya <jjuya9381@naver.com>
2017-12-04 06:56:49 +0000

    Create list controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :boards
-  
+
+  resources :lists
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
