7e0e03f09dfdeee03117afee88a394c5965e431b
jjuya <jjuya9381@naver.com>
2017-12-04 06:54:06 +0000

    Create boalist model, add associations(+deppendent)

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204064749) do
+ActiveRecord::Schema.define(version: 20171204065234) do
 
   create_table "boards", force: :cascade do |t|
     t.string   "title"
     t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "board_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
