2271b7763542d7c2588a90d00c87978cadb02206
DoYoung Kim <edwin.mucho@gmail.com>
2017-12-13 08:39:53 +0000

    merge

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -47,14 +47,19 @@ class BookmarksController < ApplicationController
   end
 
   def bookmark_params
-  	params.require(:bookmark).permit(:title, :url, :description, :list_id, :tag_1, :tag_2, :tag_3)
+  	
+  	if params[:action].eql? "create"
+  	  ext_tag(params[:bookmark][:url]).permit! 
+  	else # when Edit
+  	  params.require(:bookmark).permit(:title, :url, :description, :list_id, :tag_1, :tag_2, :tag_3)
+  	end
+  	
   end
 
-  def crawl_url
-    
-    url = params[:src_url]
+  def ext_tag(url)
+
     iskorean = false
-# ============== Make URL ==============================================================    
+   
     # If there is no http://
     unless url[/\Ahttp:\/\//] || url[/\Ahttps:\/\//]
       url = "http://#{url}"
@@ -66,13 +71,7 @@ class BookmarksController < ApplicationController
     body_name = value[1]
     tag_name = value[2]
     mobile_mode = value[3]
-    # puts title_name
-    # puts body_name
-    # puts tag_name
-    # puts mobile_mode
 
-    # #checking url
-    #mobile_mod check
     if mobile_mode
       url = url.gsub(url.partition("//")[1], url.partition("//")[1]+"m." )
     end
@@ -82,58 +81,63 @@ class BookmarksController < ApplicationController
     # #crawling
     response = HTTParty.get(url)
     doc = Nokogiri::HTML(response.body)
-    # doc = Nokogiri::HTML(open(url, :allow_redirections => :safe), nil, 'utf-8')
-    # doc = Nokogiri::HTML(open(url, :allow_redirections => :safe), nil, 'euc-kr')
-
+    
     # remove
     doc.css('script').remove
     doc.xpath("//@*[starts-with(name(),'on')]").remove
 
-    # select body
-    # title = doc.css('title').text
     title = doc.css(title_name).text
     content = doc.css(body_name).text
-    # just tag
     tag = doc.css(tag_name).text
-    # body = doc.css('body').text
-    # body.gsub!(/<\s*script\s*>|<\s*\/\s*script\s*>/, '')
-    
+
     # Checking Korean
     iskorean = true if content.match(/[가-힣]+/)
    
-    all_array = Array.new
-    # all_text = content.split(".")
     content = content.gsub("  ","")
-    all_text = content.split(/[\?\!\.;]+/)
-    p '*******************'
-    p content
-    all_text.length.times do |i|
-      l = all_text[i].strip
-      # l.gsub!(/\s+/, "")
-      if(l.length > 0)
-        # puts l
-        all_text[i] = l
-        # all_array << l
+    content = content.gsub("\n","")
+
+    if iskorean
+      all_text = content.split(/[\?\!\.;]+/)
+      all_text.length.times do |i|
+        l = all_text[i].strip
+        if(l.length > 0)
+          all_text[i] = l
+        end
       end
+      content = all_text
     end
-p '~~~~~~~~~~~~~~~~~~~~~~'
-    p all_text
-    # all_text.gsub(/<\s*script\s*>|<\s*\/\s*script\s*>/, '')
-# =======================================================================================
-# =============== English NLP ===========================================================
+
+    tag = Array.new
+
+    # twitter KoNLP / TF-IDF model
+
+    tag = iskorean ? kor_tag(content,title) : en_tag(content)
+
+    params[:bookmark][:tag_1] = tag[0]
+    params[:bookmark][:tag_2] = tag[1]
+    params[:bookmark][:tag_3] = tag[2]
     
+    return params[:bookmark]
+   
+  end
+  
+  # =============== English NLP ===========================================================
+  def en_tag(content)  
+    content = content.gsub(/[#=><]+/,"")
+    aticle = OTS.parse(content)
+    tag = Array.new
+
     # top keyword 3
-    tag = content.topics[0..2] 
-    
-    # summarize sentence
-    docu = content.summarize(percent: 20)
-    
-# =======================================================================================  
-
-# Warning It need to JAVA Install
-
+    aticle.topics[0..2].each do |t|
+      tag << t
+    end
+    return tag
+  end
+  
 # =============== Korean NLP ============================================================
-    # twitter NLP
+#     Warning It need to JAVA Install
+# ---------------------------------------------------------------------------------------
+  def kor_tag(all_text, title)
     processor = TwitterKorean::Processor.new
     # Noralize
     # twitter = processor.normalize(all_text)
@@ -174,66 +178,40 @@ p '~~~~~~~~~~~~~~~~~~~~~~'
       end
     end
     word = word.sort_by {|k, v| v}.reverse.to_h
-# =======================================================================================    
-  
-# ================== TF-IDF Model =======================================================    
-  puts all_text.class
-  puts all_text[0].class
-  idf = Array.new
-  all_text.each do |at|
-    # puts at
-    idf << TfIdfSimilarity::Document.new(at)
-    # puts idf
-  end
-  idf << TfIdfSimilarity::Document.new(title)
-
-  model = TfIdfSimilarity::TfIdfModel.new(idf, :library => :narray)
-
-  tfidf_by_term = {}
-  # idf[7].terms.each do |tff|
-  #   tfidf_by_term[tff] = model.tfidf(idf[7], tff)
-  # end
-  # idf.each do |i|
-  #   i.terms.each do |t|
-  #     tfidf_by_term[t] = model.tfidf(i, t)
-  #   end
-  # end
-  idf[-1].terms.each do |term|
-    tfidf_by_term[term] = model.tfidf(idf[-1], term)
-  end
-  tf = tfidf_by_term.sort_by{|_,tfidf| -tfidf}
-# =======================================================================================
-  # puts "+++++++++++++++++++++++++++++++++"
-  # tf.each do |tt|
-  #   puts tt
-  # end
-  # puts tf
+   
+    tag = Array.new
     
-    # puts "====="
-    # puts word
-    # puts "================="
-    # puts twitter.first.metadata.pos
-    # puts "================="
-
-    # puts "================="
-    # puts word
-    # puts "================="
-
-    tag1 = tf[-1][0]
-    tag2 = word.keys[0]
-    tag3 = word.keys[1]
-
-    Post.create(
-      title: title,
-      src_url: params[:src_url],
-      tag1: tag1,
-      tag2: tag2,
-      tag3: tag3,
-      desc: params[:desc], #대략적인 설명
-      html: all_text, #all_text, # all_arra y# text | body
-      words: word #word twitter # word # NLP fuction 비교
-      )
-    redirect_to root_path
+    tag << tf_idf(all_text, title)[-1][0]
+    tag << word.keys[0]
+    tag << word.keys[1]
+    
+    return tag
+  end
+  
+  def tf_idf(all_text, title)
+    idf = Array.new
+    all_text.each do |at|
+      # puts at
+      idf << TfIdfSimilarity::Document.new(at)
+      # puts idf
+    end
+    idf << TfIdfSimilarity::Document.new(title)
+  
+    model = TfIdfSimilarity::TfIdfModel.new(idf, :library => :narray)
+  
+    tfidf_by_term = {}
+    # idf[7].terms.each do |tff|
+    #   tfidf_by_term[tff] = model.tfidf(idf[7], tff)
+    # end
+    # idf.each do |i|
+    #   i.terms.each do |t|
+    #     tfidf_by_term[t] = model.tfidf(i, t)
+    #   end
+    # end
+    idf[-1].terms.each do |term|
+      tfidf_by_term[term] = model.tfidf(idf[-1], term)
+    end
+    tf = tfidf_by_term.sort_by{|_,tfidf| -tfidf}
   end
   
   def check_url(url)
@@ -253,12 +231,12 @@ p '~~~~~~~~~~~~~~~~~~~~~~'
 
       # "body name","tag name", "Mobile Mode"
       crawl_hash.each do |key, val|
-        p val[1]
-        p val[2]
+        # p val[1]
+        # p val[2]
         unless url[key].nil?
           val[-1] = false unless url["/m."].nil?
           #key가 crawo_hash에 있는 경우, key에 해당하는 val값이 return
-          p val
+          # p val
           return val
         end
       end
